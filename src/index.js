63683ac83abcde274c9430aef70bc5b248afa2dc
oxanadev <shana_bj@hotmail.com>
2024-11-30 16:28:43 +0500

    Added StarRating

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
+    <StarRating
+      maxRating={"hi"}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="pink" className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
